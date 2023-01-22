11d623274f24d171285f6e331bc83fed42f350c3
Jacob Franz <jacobwfranz@gmail.com>
2023-01-22 12:46:26 -0500

    Set up CocoaPods and SwiftLint

---
+++ Movie Mentor/Pods/Target Support Files/Pods-Movie Mentor-Movie MentorUITests/Pods-Movie Mentor-Movie MentorUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Movie_Mentor_Movie_MentorUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Movie_Mentor_Movie_MentorUITests
+@end
---
+++ Movie Mentor/Pods/Target Support Files/Pods-Movie Mentor/Pods-Movie Mentor-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Movie_Mentor : NSObject
+@end
+@implementation PodsDummy_Pods_Movie_Mentor
+@end
