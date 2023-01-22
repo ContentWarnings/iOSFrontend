96d67494d9739ccea74abbeb37eb204b800a9ed8
Jacob Franz <jacobwfranz@gmail.com>
2023-01-22 12:15:45 -0500

    Initial app setup

---
+++ Movie Mentor/Movie Mentor/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Movie Mentor
+//
+//  Created by Jacob Franz on 1/22/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Movie Mentor/Movie Mentor/Movie_MentorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Movie_MentorApp.swift
+//  Movie Mentor
+//
+//  Created by Jacob Franz on 1/22/23.
+//
+
+import SwiftUI
+
+@main
+struct Movie_MentorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
