40192110bfdabdf770c61f485017399ce0c31861
Jacob Franz <jacobwfranz@gmail.com>
2023-02-13 21:22:21 -0500

    Create GenreTileView

---
+++ Movie Mentor/Movie Mentor/Views/Components/GenreTileView.swift
@@ -0,0 +1,54 @@
+import SwiftUI
+
+struct GenreTileView: View {
+    let genre: String
+    
+    var body: some View {
+        ZStack {
+            Image("Action")
+                .resizable()
+                .scaledToFill()
+                .frame(height: 90.0)
+                .clipped()
+                .cornerRadius(11)
+            Rectangle()
+                .frame(height: 90.0)
+                .foregroundColor(Color("OLEDDark").opacity(0.75))
+                .cornerRadius(11)
+            Text(genre)
+                .font(Font.custom("Roboto-Bold", size: 42))
+                .foregroundColor(Color("PerfectWhite"))
+        }
+    }
+}
+
+struct GenreTileView_Previews: PreviewProvider {
+    static var previews: some View {
+        GenreTileView(genre: "Action")
+    }
+}
+
+extension GenreTileView {
+    static let allGenreNames =
+    [
+        "Action",
+        "Adventure",
+        "Animation",
+        "Comedy",
+        "Crime",
+        "Documentary",
+        "Drama",
+        "Family",
+        "Fantasy",
+        "History",
+        "Horror",
+        "Music",
+        "Mystery",
+        "Romance",
+        "Science Fiction",
+        "TV Movie",
+        "Thriller",
+        "War",
+        "Western"
+    ]
+}
