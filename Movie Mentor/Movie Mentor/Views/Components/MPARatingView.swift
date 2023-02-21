71d8da84461c05e5e5143ba34b5ae9d0290e59fd
Jacob Franz <jacobwfranz@gmail.com>
2023-02-21 16:06:58 -0500

    Create MPARatingView

---
+++ Movie Mentor/Movie Mentor/Views/Components/MPARatingView.swift
@@ -0,0 +1,41 @@
+import SwiftUI
+
+struct MPARatingView: View {
+    let rating: String
+    var description: String {
+        switch rating {
+        case "G":
+            return "General Audiences"
+        case "PG":
+            return "Parental Guidance Suggested"
+        case "PG-13":
+            return "Parents Strongly Cautioned"
+        case "R":
+            return "Restricted"
+        case "NC-17":
+            return "Adults Only"
+        default:
+            return "Rating Unknown"
+        }
+    }
+    
+    var body: some View {
+        HStack {
+            Text(rating)
+                .font(Font.custom("Roboto-Bold", size: 28))
+                .padding(8.0)
+                .border(.primary, width: 3)
+                .cornerRadius(3)
+            Text(description)
+                .font(Font.custom("Roboto-Regular", size: 21))
+            Spacer()
+        }
+        .padding(.horizontal, 25.0)
+    }
+}
+
+struct MPARatingView_Previews: PreviewProvider {
+    static var previews: some View {
+        MPARatingView(rating: "PG-13")
+    }
+}
