4396a4f8cba15f22e12710dadd9bb25c49fd417f
Jacob Franz <jacobwfranz@gmail.com>
2023-02-27 15:23:30 -0500

    Create ContentWarning model

---
+++ Movie Mentor/Movie Mentor/Models/ContentWarning.swift
@@ -0,0 +1,26 @@
+import Foundation
+
+struct ContentWarning: Identifiable {
+    var id: String
+    var name: String
+    var times: [(Int, Int)]
+    var description: String
+}
+
+extension ContentWarning {
+    static let testData: [ContentWarning] =
+    [
+        ContentWarning(id: "cc8ea1b8-2155-4e35-97b5-cda4058edc0e",
+                       name: "Murder",
+                       times: [(1, 2), (3, 4)],
+                       description: "Blue people die."),
+        ContentWarning(id: "cc8ea1b8-2155-4e35-97b5-cda4058acd3",
+                       name: "Kidnapping",
+                       times: [(1, 2), (3, 4)],
+                       description: "Blue people are kidnapped."),
+        ContentWarning(id: "cc8ea1b8-2155-4e35-97b5-cda4058edabde",
+                       name: "Drug Use",
+                       times: [(1, 2), (3, 4)],
+                       description: "Blue people use drugs."),
+    ]
+}
